0e012dc300e4b80ac17f72fe49c38a4ad8e55048
Jannis R <mail@jannisr.de>
2017-09-06 12:18:16 +0200

    status API

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 'use strict'
 
-// todo
+const qs = require('querystring')
+const {fetch} = require('fetch-ponyfill')({Promise: require('pinkie-promise')})
+const omit = require('lodash.omit')
+
+const endpoint = 'https://portal.imice.de/api1/rs/'
+const userAgent = 'https://github.com/derhuerst/wifi-on-ice-portal-client'
+
+const request = (route, query) => {
+	if ('string' !== typeof route) throw new Error('route must be a string')
+	if ('object' !== typeof query) throw new Error('query must be an object')
+
+	const url = endpoint + route + '?' + qs.stringify(query)
+	return fetch(url, {
+		mode: 'cors',
+		redirect: 'follow',
+		headers: {'User-Agent': userAgent}
+	})
+	.then((res) => {
+		if (!res.ok) {
+			const err = new Error(res.statusText)
+			err.statusCode = res.status
+			throw err
+		}
+		return res.json()
+	})
+}
+
+const status = () => {
+	return request('status', {})
+	.then((data) => {
+		const res = omit(data, ['connection', 'gpsStatus'])
+		res.ok = data.connection
+		res.gpsOk = data.gpsStatus === 'VALID'
+		return res
+	})
+}
+
+module.exports = {status}
--- test.js
+++ test.js
@@ -1,8 +1,25 @@
 'use strict'
 
 const test = require('tape')
+const isRoughlyEqual = require('is-roughly-equal')
 
-const todo = require('.')
+const portal = require('.')
 
-test('todo', (t) => {
+const minute = 60 * 1000
+
+test('status', (t) => {
+	t.plan(8)
+
+	portal.status()
+	.then((data) => {
+		t.equal(data.ok, true)
+		t.equal(typeof data.speed, 'number')
+		t.ok(data.speed >= 0)
+		t.ok(data.speed < 500)
+		t.equal(data.gpsOk, true)
+		t.ok(isRoughlyEqual(10, data.latitude, 49))
+		t.ok(isRoughlyEqual(10, data.longitude, 10))
+		t.ok(isRoughlyEqual(10 * minute, data.serverTime, Date.now()))
+	})
+	.catch(t.ifError)
 })
