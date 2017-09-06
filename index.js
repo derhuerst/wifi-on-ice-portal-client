1a888919a749653882fde9400c6343af1401fc1f
Jannis R <mail@jannisr.de>
2017-09-06 13:20:45 +0200

    tripInfo API

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const qs = require('querystring')
 const {fetch} = require('fetch-ponyfill')({Promise: require('pinkie-promise')})
 const omit = require('lodash.omit')
+const moment = require('moment-timezone')
+const slugg = require('slugg')
 
 const endpoint = 'https://portal.imice.de/api1/rs/'
 const userAgent = 'https://github.com/derhuerst/wifi-on-ice-portal-client'
@@ -37,4 +39,79 @@ const status = () => {
 	})
 }
 
-module.exports = {status}
+const parseTimestamp = (t) => {
+	return moment(t).tz('Europe/Berlin').format()
+}
+
+const parsePassed = (_) => {
+	const t = _.timetable
+
+	const arr = t.actualArrivalTime
+	const schedArr = t.scheduledArrivalTime
+	const arrDelay = arr && schedArr ? Math.round((arr - schedArr) / 1000) : null
+
+	const dep = t.actualDepartureTime
+	const schedDep = t.scheduledDepartureTime
+	const depDelay = dep && schedDep ? Math.round((dep - schedDep) / 1000) : null
+
+	const platform = _.track.actual || _.track.scheduled || null
+
+	// todo: what is _.info.status?
+	// todo: _.info.distance
+	// todo: _.delayReasons
+	return {
+		station: {
+			type: 'station',
+			// todo: the ids looks like "8000169_00", remove last part?
+			id: _.station.evaNr,
+			name: _.station.name,
+			coordinates: _.station.geocoordinates
+		},
+		arrival: arr || schedArr ? parseTimestamp(arr || schedArr) : null,
+		arrivalDelay: arrDelay,
+		arrivalPlatform: platform,
+		departure: dep || schedDep ? parseTimestamp(dep || schedDep) : null,
+		departureDelay: depDelay,
+		departurePlatform: platform,
+		passed: _.info.passed,
+		kmFromStart: _.info.distanceFromStart / 1000
+	}
+}
+
+const journey = () => {
+	return request('tripInfo', {})
+	.then((_) => {
+		const passed = _.stops.map(parsePassed)
+
+		const lineName = _.trainType + ' ' + _.vzn
+		const lineId = slugg(lineName)
+
+		const s = _.stopInfo
+		const findPassed = (prop) => {
+			const p = s[prop] && passed.find(p => p.station.id === s[prop])
+			return p && p.station || null
+		}
+		const next = findPassed('actualNext')
+		const scheduledNext = findPassed('scheduledNext')
+		const previous = findPassed('actualLast')
+		const last = findPassed('finalStationEvaNr')
+
+		return {
+			// todo: id
+			public: true,
+			mode: 'train',
+			line: {
+				type: 'line',
+				id: lineId,
+				name: lineName
+			},
+			// todo: _.actualPosition, _.totalDistance
+			next, scheduledNext,
+			previous,
+			last,
+			passed
+		}
+	})
+}
+
+module.exports = {status, journey}
--- test.js
+++ test.js
@@ -7,6 +7,10 @@ const portal = require('.')
 
 const minute = 60 * 1000
 
+const isValidDate = (d) => {
+	return 'string' === typeof d && !Number.isNaN(+new Date(d))
+}
+
 test('status', (t) => {
 	t.plan(8)
 
@@ -20,6 +24,70 @@ test('status', (t) => {
 		t.ok(isRoughlyEqual(10, data.latitude, 49))
 		t.ok(isRoughlyEqual(10, data.longitude, 10))
 		t.ok(isRoughlyEqual(10 * minute, data.serverTime, Date.now()))
+	})
+	.catch(t.ifError)
+})
+
+test('journey', (t) => {
+	portal.journey()
+	.then((leg) => {
+		t.equal(leg.public, true)
+		t.equal(leg.mode, 'train')
+
+		t.ok(leg.line)
+		t.equal(leg.line.type, 'line')
+		t.equal(typeof leg.line.id, 'string')
+		t.ok(leg.line.id)
+		t.equal(typeof leg.line.name, 'string')
+		t.ok(leg.line.name)
+
+		if (leg.next) t.equal(leg.next.type, 'station')
+		if (leg.previous) t.equal(leg.previous.type, 'station')
+		t.ok(leg.last)
+		t.equal(leg.last.type, 'station')
+
+		t.ok(Array.isArray(leg.passed))
+		for (let i = 0; i < leg.passed.length; i++) {
+			const p = leg.passed[i]
+			const msg = 'passed ' + i
+			t.ok(p, msg)
+
+			t.ok(p.station, msg)
+			t.equal(typeof p.station.id, 'string', msg)
+			t.ok(p.station.id, msg)
+			t.equal(typeof p.station.name, 'string', msg)
+			t.ok(p.station.name, msg)
+
+			const c = p.station.coordinates
+			t.ok(c, msg)
+			t.ok(isRoughlyEqual(10, c.latitude, 49), msg)
+			t.ok(isRoughlyEqual(10, c.longitude, 10), msg)
+
+			if (p.arrival) t.ok(isValidDate(p.arrival), msg)
+			if (p.departure) t.ok(isValidDate(p.departure), msg)
+
+			if (p.arrivalDelay !== null) {
+				t.equal(typeof p.arrivalDelay, 'number', msg)
+			}
+			if (p.departureDelay !== null) {
+				t.equal(typeof p.departureDelay, 'number', msg)
+			}
+
+			if (p.arrivalPlatform) {
+				t.equal(typeof p.arrivalPlatform, 'string', msg)
+				t.ok(p.arrivalPlatform, msg)
+			}
+			if (p.departurePlatform) {
+				t.equal(typeof p.departurePlatform, 'string', msg)
+				t.ok(p.departurePlatform, msg)
+			}
+
+			t.equal(typeof p.passed, 'boolean', msg)
+			t.equal(typeof p.kmFromStart, 'number', msg)
+			t.ok(p.kmFromStart >= 0, msg)
+		}
+
+		t.end()
 	})
 	.catch(t.ifError)
 })
